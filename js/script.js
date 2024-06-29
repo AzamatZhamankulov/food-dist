a9de61a274ab77c4e0302fe5a66fcf2f2c2c73ac
Azamat Zhamankulov <azamat.zhamankulov.kk@gmail.com>
2024-06-29 19:56:10 +0600

    Done the tab functionalities hide and show the content

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 window.addEventListener('DOMContentLoaded', () => {
 
     const tabs = document.querySelectorAll('.tabheader__item'),
@@ -5,9 +7,43 @@ window.addEventListener('DOMContentLoaded', () => {
           tabsParent = document.querySelector('.tabheader__items');
 
 
+    // function hideTabContent() {
+    //     tabsContent.forEach(item => {
+    //         item.style.display = 'none';
+    //     });
+
+    //     tabs.forEach(item => {
+    //         item.classList.remove('tabheader__item_active');
+    //     });
+    // }
+
+    // function showTabContent(i = 0) {
+    //     tabsContent[i].style.display ='block';
+    //     tabs[i].classList.add('tabheader__item_active');
+    // }
+
+    // hideTabContent();
+    // showTabContent();
+
+    // tabsParent.addEventListener('click', (e) => {
+    //     const target = e.target;
+
+    //     if (target && target.classList.contains('tabheader__item')) {
+    //         tabs.forEach((item, i) => {
+    //             if (target == item) {
+    //                 hideTabContent();
+    //                 showTabContent(i);
+    //             }
+    //         });
+    //     }
+    // });
+
+    //todo we have added classes in style.css so we can change functions with new classes
+
     function hideTabContent() {
         tabsContent.forEach(item => {
-            item.style.display = 'none';
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
         });
 
         tabs.forEach(item => {
@@ -15,14 +51,26 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    function showTabContent(i) {
-        tabsContent[i].style.display ='block';
-        item.classList.remove('tabheader__item_active');
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
     }
 
+    hideTabContent();
+    showTabContent();
 
+    tabsParent.addEventListener('click', (e) => {
+        const target = e.target;
 
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
 
-
-
-})
+});
