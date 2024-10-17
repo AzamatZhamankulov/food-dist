de18daaf70edf7e0ecdb26417c0fc0b974db0f09
Azamat Zhamankulov <azamat.zhamankulov.kk@gmail.com>
2024-10-17 19:34:50 +0600

    Added sending data by JSON object before it was XMLhttprequest

--- js/script.js
+++ js/script.js
@@ -300,10 +300,17 @@ window.addEventListener('DOMContentLoaded', () => {
             const request = new XMLHttpRequest();
             request.open('POST', 'server.php');
 
-            // request.setRequestHeader('Content-type', 'multipart/form-data'); // it is not neccessary
+            request.setRequestHeader('Content-type', 'application/json'); // it is not neccessary if it is not JSON
             const formData = new FormData(form);
 
-            request.send(formData);
+            const object = {};
+            formData.forEach(function(value, key){
+                object[key] = value;
+            });
+
+            const json = JSON.stringify(object);
+
+            request.send(json);
 
             request.addEventListener('load', () => {
                 if (request.status === 200) {
